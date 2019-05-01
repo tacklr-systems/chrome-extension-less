bbfa5a640d3780e7fed9024b61eac08543c1db2f
Jason Martin <jason@digital-pioneers.com>
2019-05-01 16:53:24 -0400

    add url support

--- src/js/content.js
+++ src/js/content.js
@@ -1,32 +1,59 @@
-var less_sheets = [];
+var lessSheets = [];
+var promises = [];
+var lessIndex = 0;
 
 $('style').each(function(idx, val)
 {
 	if ($(val).attr('type') == 'text/less')
 	{
-		var id = $(val).attr('id')? $(val).attr('id') : 'less-stylesheet-' + (less_sheets.length + 1);
 
-		less_sheets.push({'id': id, 'less': $(val).text()});
+		var id = $(val).attr('id')? $(val).attr('id') : 'less-stylesheet-' + (++lessIndex);
+
+		lessSheets.push({'id': id, 'less': $(val).text()});
 
 		$(val).attr({'id': id});
 	}
 });
 
-chrome.extension.sendRequest(less_sheets, function(response) 
+$('link').each(function(idx, val)
 {
-	$(response).each(function(idx, val)
+	if ($(val).attr('type') == 'text/less')
 	{
-		if (typeof val.css !== "undefined")
+		var url = $(val).attr('href');
+		var id = $(val).attr('id')? $(val).attr('id') : 'less-stylesheet-' + (++lessIndex);
+
+		var p = $.get( url, function( data ) 
 		{
-			$('#' + val.id).after($('<style />')
-				.text(val.css)
-				.attr({'type': "text/css", 'data-less-stylesheet-id': val.id})
-			);	
-		}
-		else
+			lessSheets.push({'id': id, 'less': data });
+		});
+		promises.push(p);
+
+		$(val).attr({'id': id});
+	}
+});
+
+Promise.all(promises)
+.then(() =>
+{
+	if(lessSheets.length > 0)
+	{
+		chrome.extension.sendRequest(lessSheets, function(response) 
 		{
-			console.error(val.error);
-		}
-		
-	});
+			$(response).each(function(idx, val)
+			{
+				if (typeof val.css !== "undefined")
+				{
+					$('#' + val.id).after($('<style />')
+						.text(val.css)
+						.attr({'type': "text/css", 'data-less-stylesheet-id': val.id})
+					);	
+				}
+				else
+				{
+					console.error(val.error);
+				}
+				
+			});
+		});
+	}
 });
